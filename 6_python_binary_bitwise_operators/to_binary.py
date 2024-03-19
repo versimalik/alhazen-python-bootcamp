814c6468bcbcc9112b50cb9e2894dab564955b73
aldiafrizarman <71926031+aldiafrizarman@users.noreply.github.com>
2024-03-19 22:05:45 +0700

    Day 2 - Binary & Bitwise Operators

---
+++ 6_python_binary_bitwise_operators/to_binary.py
@@ -0,0 +1,6 @@
+number1 = 100
+
+# Gunakan function bin() untuk mengonversi desimal ke binary
+to_binary = bin(number1)
+
+print(to_binary)
---
+++ 6_python_binary_bitwise_operators/to_decimal.py
@@ -0,0 +1,8 @@
+binary_num = '10011'
+
+# Gunakan function int() untuk mengonversi binary ke decimal
+to_decimal = int(binary_num, 2)
+
+print(to_decimal)
+
+# Output = 19
